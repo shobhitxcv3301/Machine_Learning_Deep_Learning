751a336d594a40b501bd992dceff74e106cbab11
Shobhit Srivastav <shobhitxcv3301@gmail.com>
2025-03-05 12:34:58 +0530

    decisionn tree

---
+++ webapp/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/Landingpage')
+def Landingpage():
+    return render_template('Landingpage.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
